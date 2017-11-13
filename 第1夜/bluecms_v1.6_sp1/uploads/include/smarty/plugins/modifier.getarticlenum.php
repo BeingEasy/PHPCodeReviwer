61664c5fba75922e7a4481cf4232e8ea067ac0f1
arch3rPro <30855883+arch3rPro@users.noreply.github.com>
2017-11-14 02:20:03 +0800

    del

--- 第1夜/bluecms_v1.6_sp1/uploads/include/smarty/plugins/modifier.getarticlenum.php
+++
@@ -1,9 +0,0 @@
-<?php
-function smarty_modifier_getarticlenum($string)
-{
-	global $db;
-	$sql="select count(*) from lucks_article where catid='$string'";
-	$result=$db->select($sql);
-	return $result[0]['count(*)'];
-}
-?>
